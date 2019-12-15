a1a07a14f552e458e9e708d0ed9bd0391e6b706e
이희영 <gl171111@gmail.com>
2019-12-15 11:35:46 +0900

    데이터베이스 생성 및 ASP.NET MVC Web API 개발환경 설정

---
+++ WebDiskApplication/Areas/WebDisk/WebDiskAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebDiskApplication.Areas.WebDisk
+{
+    public class WebDiskAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WebDisk";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WebDisk_default",
+                "WebDisk/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
